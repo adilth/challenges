75b851f9fc1779a850fd5d12cc16c9defee6da77
adil tahiri <rajaadil1995@gmail.com>
2022-04-10 00:58:12 +0000

    add some explanation

--- challenge12.js
+++ challenge12.js
@@ -23,6 +23,15 @@ console.log("Sum= " + calculator.sum());
 console.log("Mul= " + calculator.mul());
 console.log("Mul= " + calculator.subs());
 
+/*
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+Hint for R users:
+
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
+*/
+
 function shortLong(a, b) {
   if (a.length < b.length) {
     return a + b + a;
@@ -30,9 +39,24 @@ function shortLong(a, b) {
     return b + a + b;
   }
 }
+
 // or const shortLongShort = (a,b)=> a.length < b.length ? a + b + a : b + a + b
 console.log(shortLong("1", "22")); // --> "1221"
 console.log(shortLong("22", "1")); // --> "1221
+
+/*
+write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+the string should start with a 1.
+
+a string with size 6 should return :'101010'.
+
+with size 4 should return : '1010'.
+
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.
+*/
 
 function biniry(str) {
   let string = ``;
