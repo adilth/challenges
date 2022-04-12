fef2c4a7f87c8bec49e15e0de6ec721033cdbbc3
adil tahiri <rajaadil1995@gmail.com>
2022-04-12 01:06:07 +0000

    new challenge

---
+++ challenge14.js
@@ -0,0 +1,11 @@
+//Find the vowels
+// function findVowels(str){
+// let vowelsCount = 0;
+// for (let char of str) {
+//   if (vowels.includes(char)) {
+//     vowelsCount++;
+//   }
+// }
+//}
+
+// return vowelsCount;
