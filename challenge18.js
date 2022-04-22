b9a25e4a0344d0a3f411b7dc30c5ac340c7b7e20
adil tahiri <rajaadil1995@gmail.com>
2022-04-22 00:22:34 +0000

    like social is it's sqrt betterthenavrge fristcapital

--- challenge18.js
+++ challenge18.js
@@ -70,3 +70,19 @@ function betterThanAverage(classPoints, yourPoints) {
 }
 
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75));
+
+/*
+Description
+Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. make the first character in the string "word" upper case).
+
+Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
+*/
+function capitalizeWord(word) {
+  // return word[0].toUpperCase() + word.slice(1);
+  //or this
+
+  let splitWord = word.split("");
+  return splitWord[0].toUpperCase() + splitWord.slice(1).join("");
+}
