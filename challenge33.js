6041181c359ccf8240a9f23086c5035bc59c13ee
adil tahiri <rajaadil1995@gmail.com>
2022-05-16 17:23:55 +0100

    add 7kyu removes the firt and last character from a string

---
+++ challenge33.js
@@ -0,0 +1,14 @@
+/* Description 7kyu //
+Description
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+*/
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+console.log(removeChar("why there is no character"));
+
+//output : hy there is no characte
