455274a6b97a23e8e2edb69f44a84ce2532cf63b
adil tahiri <rajaadil1995@gmail.com>
2022-05-21 02:21:58 +0100

    add 8kyu Grasshopper - Summation sum every digit from 1 to num

---
+++ challenge39.js
@@ -0,0 +1,30 @@
+/*Description 8kyu //Grasshopper - Summation
+
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+*/
+
+let summation = function (num) {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+
+  return result;
+};
+
+// function summation(num) {
+//   return (num * (num + 1)) / 2;
+// }
+console.log(summation(30));
+
+//output : 456
