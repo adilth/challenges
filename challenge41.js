0cb613cce1591a13bee6491e7d4a7246177dc5f4
adil tahiri <rajaadil1995@gmail.com>
2022-05-22 02:03:09 +0100

    add 6kyu create phone number replace all number with # and last 4 still same

---
+++ challenge41.js
@@ -0,0 +1,31 @@
+/*Description 6kyu  // Create Phone Number
+Description
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+Don't forget the space after the closing parentheses!
+*/
+
+function solution(arr) {
+  let first = arr.slice(0, 3).join("");
+  let second = arr.slice(3, 6).join("");
+  let third = arr.slice(6, arr.length).join("");
+
+  return `(${first}) ${second}-${third}`;
+}
+
+//good solution
+function solution(num) {
+  var format = "(xxx) xxx-xxxx";
+
+  for (var i = 0; i < num.length; i++) {
+    format = format.replace("x", num[i]);
+  }
+
+  return format;
+}
+console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+//output : "(123) 456-7890"
