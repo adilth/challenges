ec00201ba113ad2fb5d458b80242be3a2daace64
adil tahiri <rajaadil1995@gmail.com>
2022-05-24 00:12:57 +0100

    add 7kyu sum two smallest nums

---
+++ challenge43.js
@@ -0,0 +1,19 @@
+/* Description //7kyu
+ Description
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+*/
+
+function sumTwoSmallestNumbers(numbers) {
+  let [first, second] = numbers.sort((a, b) => a - b);
+  return first + second;
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]));
+
+//output :7
+//3453455
