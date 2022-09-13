40e47b230a1c200d8d8fd69015e193c86d132bb2
adil tahiri <rajaadil1995@gmail.com>
2022-09-13 21:11:48 +0100

    add 6kyu converting duplicate char to ( other to )

---
+++ challenge107.js
@@ -0,0 +1,33 @@
+/* Description // 6kyu //Duplicate Encoder
+
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+*/
+
+function changeDuplicate(word) {
+  let arr = word.toLowerCase().split("");
+  let dup = arr.filter((item, idx) => arr.indexOf(item) !== idx);
+  return arr.map((el) => (dup.includes(el) ? ")" : "(")).join("");
+}
+
+//other solution
+
+// function changeDuplicate(word) {
+//   return word
+//     .toLowerCase()
+//     .split("")
+//     .map(function (a, _, w) {
+//       return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+//     })
+//     .join("");
+// }
+console.log(changeDuplicate("encesn")); //))()()
+console.log(changeDuplicate("recede")); //()()()
+console.log(changeDuplicate("adsalojsjkltkder")); //)))))()))))())((
